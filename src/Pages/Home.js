00c3982679e290429b6ccdc671ae80598366495c
RyanAndroid <49736356+RyanAndroid@users.noreply.github.com>
2022-06-23 02:42:26 -0400

    edited css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,18 +10,14 @@ import selfie from '../Images/selfie.jpg';
 
 function Home() {
   return (
-    <div className="home-border">
+    <div>
       <img className='selfie' src={selfie}></img>
 
       <div className='biography'>
         Hello! My name is Ryan Chiu. 
-        <br />
-        <br />
         Can you guess what major I am in? If you look at the reflection of my glasses, you will see that I have 
         VS Code saved as a bookmark! This implies that I am a CS major! I study Computer Science at the University of Waterloo and currently, 
         I am in my 2B term. In my free time, I like to play piano, chess, and read books.  
-        <br />
-        <br />
         Hopefully you will get to know a little more about me
         through this website! Fun fact: I used VS Code to code and design this website :)
       </div>
@@ -30,7 +26,6 @@ function Home() {
         <a className="site-link1" href="https://www.linkedin.com/in/ryan-chiu-835745205/">LinkedIn</a>
         <a className="site-link2" href="https://github.com/ryan314?tab=repositories">GitHub</a> 
       </div>
-      
     </div>
   );
 }
